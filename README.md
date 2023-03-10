db5e97cf284a860974a353888a7a170989ad70c7
miao <wang_m1002@163.com>
2023-03-11 00:39:26 +0800

    Initial scripts for building Parcel and CSD (#1)
    undefined

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# kyuubi-parcel
+# kyuubi-parcel and csd make script
+
+This project script must be executed in the bin directory, otherwise an error will occur
+
+After the script is executed, the parcel file and csd file for deployment will be generated in the directory parcel/parcel_out and csd/csd_out
---
+++ csd/kyuubi-csd-src/scripts/control.sh
@@ -0,0 +1,72 @@
+#!/bin/bash
+set -x
+case $1 in
+  (start)
+    export KYUUBI_HOME
+    export KYUUBI_CONF_DIR=$CONF_DIR/conf
+    export KYUUBI_LOG_DIR=$KYUUBI_LOG_DIR
+    export KYUUBI_PID_DIR=$CONF_DIR/pid
+    export KYUUBI_WORK_DIR_ROOT=$KYUUBI_WORK_DIR
+    while read line; do
+      logline=$(echo $line | grep "log.threshold=" || true)
+      if [[ "$logline" != "" ]]
+      then
+        logevel=${logline#*log.threshold=}
+			  continue
+      fi
+      logline=$(echo $line | grep "log.file=" || true)
+      if [[ "$logline" != "" ]]
+      then
+        logfile=${logline#*log.file=}
+			  continue
+      fi
+      logline=$(echo $line | grep "max.log.file.size=" || true)
+      if [[ "$logline" != "" ]]
+      then
+        maxsize=${logline#*max.log.file.size=}
+			  continue
+      fi
+      logline=$(echo $line | grep "max.log.file.backup.index=" || true)
+      if [[ "$logline" != "" ]]
+      then
+        maxindex=${logline#*max.log.file.backup.index=}
+			  continue
+      fi
+    done < log4j.properties
+    echo status=${logevel} >> conf/log4j2.properties
+    echo name=KyuubiLog4j2 >> conf/log4j2.properties
+    echo property.log.level=${logevel} >> conf/log4j2.properties
+    echo rootLogger.level=\${log.level} >> conf/log4j2.properties
+    echo rootLogger.appenderRefs=console,DRFA >> conf/log4j2.properties
+    echo rootLogger.appenderRef.console.ref=console >> conf/log4j2.properties
+    echo rootLogger.appenderRef.console.level=ERROR >> conf/log4j2.properties
+    echo rootLogger.appenderRef.DRFA.ref=DRFA >> conf/log4j2.properties
+    echo rootLogger.appenderRef.DRFA.level=${logevel} >> conf/log4j2.properties
+    echo property.log.dir=${KYUUBI_LOG_DIR} >> conf/log4j2.properties
+    echo property.log.file=${logfile} >> conf/log4j2.properties
+    echo appenders=console,DRFA >> conf/log4j2.properties
+    echo property.max.log.file.size=${maxsize} >> conf/log4j2.properties
+    echo property.max.log.file.backup.index=${maxindex} >> conf/log4j2.properties
+    echo appender.DRFA.type=RollingRandomAccessFile >> conf/log4j2.properties
+    echo appender.DRFA.name=DRFA >> conf/log4j2.properties
+    echo appender.DRFA.fileName=\${log.dir}/\${log.file} >> conf/log4j2.properties
+    echo appender.DRFA.filePattern=\${log.dir}/\${log.file}.%d{yyyy-MM-dd} >> conf/log4j2.properties
+    echo appender.DRFA.layout.type=PatternLayout >> conf/log4j2.properties
+    echo appender.DRFA.layout.pattern=%d{DEFAULT} %-5p %c: [%t]: %m%n >> conf/log4j2.properties
+    echo appender.DRFA.policies.type=Policies >> conf/log4j2.properties
+    echo appender.DRFA.policies.size.type=SizeBasedTriggeringPolicy >> conf/log4j2.properties
+    echo appender.DRFA.policies.size.size=\${max.log.file.size} >> conf/log4j2.properties
+    echo appender.DRFA.strategy.type=DefaultRolloverStrategy >> conf/log4j2.properties
+    echo appender.DRFA.strategy.max=\${max.log.file.backup.index} >> conf/log4j2.properties
+    echo appender.console.type=Console >> conf/log4j2.properties
+    echo appender.console.name=console >> conf/log4j2.properties
+    echo appender.console.target=SYSTEM_ERR >> conf/log4j2.properties
+    echo appender.console.layout.type=PatternLayout >> conf/log4j2.properties
+    echo appender.console.layout.pattern=%d{yy/MM/dd HH:mm:ss} %-5p %c{2}: [%t]: %m%n >> conf/log4j2.properties
+    exec ${KYUUBI_HOME}/bin/kyuubi run
+    ;;
+  (*)
+    echo "Don't understand [$1]"
+    exit 1
+    ;;
+esac
