668208b794f67b532776d9ffab3424073fdf0f91
Cheng Pan <chengpan@apache.org>
2023-03-13 15:22:12 +0800

    template for log4j2.configuration

--- csd/scripts/control.sh
+++ csd/scripts/control.sh
@@ -1,65 +1,39 @@
 #!/bin/bash
 set -x
+
+# Read a value from a properties file.
+function read_property {
+  local key="$1"
+  local file="$2"
+  echo $(grep "^$key=" "$file" | tail -n 1 | sed "s/^$key=\(.*\)/\\1/")
+}
+
+# replace $1 with $2 in file $3
+function replace {
+  perl -pi -e "s#${1}#${2}#g" $3
+}
+
+function prepare_log_conf {
+  local KYUUBI_LOG_LEVEL=$(                read_property "log.threshold"             "$CONF_DIR/log4j.properties")
+  local KYUUBI_LOG_DIR=$(                  read_property "log.dir"                   "$CONF_DIR/log4j.properties")
+  local KYUUBI_LOG_FILE=$(                 read_property "log.file"                  "$CONF_DIR/log4j.properties")
+  local KYUUBI_MAX_LOG_FILE_SIZE=$(        read_property "max.log.file.size"         "$CONF_DIR/log4j.properties")
+  local KYUUBI_MAX_LOG_FILE_BACKUP_INDEX=$(read_property "max.log.file.backup.index" "$CONF_DIR/log4j.properties")
+  
+  cat $CONF_DIR/aux/log4j2.properties.template > $CONF_DIR/conf/log4j2.properties
+  
+  replace "{{KYUUBI_LOG_LEVEL}}"                 "$KYUUBI_LOG_LEVEL"                 "$CONF_DIR/conf/log4j2.properties"
+  replace "{{KYUUBI_LOG_DIR}}"                   "$KYUUBI_LOG_DIR"                   "$CONF_DIR/conf/log4j2.properties"
+  replace "{{KYUUBI_LOG_FILE}}"                  "$KYUUBI_LOG_FILE"                  "$CONF_DIR/conf/log4j2.properties"
+  replace "{{KYUUBI_MAX_LOG_FILE_SIZE}}"         "$KYUUBI_MAX_LOG_FILE_SIZE"         "$CONF_DIR/conf/log4j2.properties"
+  replace "{{KYUUBI_MAX_LOG_FILE_BACKUP_INDEX}}" "$KYUUBI_MAX_LOG_FILE_BACKUP_INDEX" "$CONF_DIR/conf/log4j2.properties"
+}
+
 case $1 in
   (start)
     export KYUUBI_HOME=${KYUUBI_HOME:-$CDH_KYUUBI_HOME}
     export KYUUBI_CONF_DIR=${KYUUBI_CONF_DIR:-$CONF_DIR/conf}
-    while read line; do
-      logline=$(echo $line | grep "log.threshold=" || true)
-      if [[ "$logline" != "" ]]
-      then
-        logevel=${logline#*log.threshold=}
-			  continue
-      fi
-      logline=$(echo $line | grep "log.file=" || true)
-      if [[ "$logline" != "" ]]
-      then
-        logfile=${logline#*log.file=}
-			  continue
-      fi
-      logline=$(echo $line | grep "max.log.file.size=" || true)
-      if [[ "$logline" != "" ]]
-      then
-        maxsize=${logline#*max.log.file.size=}
-			  continue
-      fi
-      logline=$(echo $line | grep "max.log.file.backup.index=" || true)
-      if [[ "$logline" != "" ]]
-      then
-        maxindex=${logline#*max.log.file.backup.index=}
-			  continue
-      fi
-    done < log4j.properties
-    echo status=${logevel} >> conf/log4j2.properties
-    echo name=KyuubiLog4j2 >> conf/log4j2.properties
-    echo property.log.level=${logevel} >> conf/log4j2.properties
-    echo rootLogger.level=\${log.level} >> conf/log4j2.properties
-    echo rootLogger.appenderRefs=console,DRFA >> conf/log4j2.properties
-    echo rootLogger.appenderRef.console.ref=console >> conf/log4j2.properties
-    echo rootLogger.appenderRef.console.level=ERROR >> conf/log4j2.properties
-    echo rootLogger.appenderRef.DRFA.ref=DRFA >> conf/log4j2.properties
-    echo rootLogger.appenderRef.DRFA.level=${logevel} >> conf/log4j2.properties
-    echo property.log.dir=${KYUUBI_LOG_DIR} >> conf/log4j2.properties
-    echo property.log.file=${logfile} >> conf/log4j2.properties
-    echo appenders=console,DRFA >> conf/log4j2.properties
-    echo property.max.log.file.size=${maxsize} >> conf/log4j2.properties
-    echo property.max.log.file.backup.index=${maxindex} >> conf/log4j2.properties
-    echo appender.DRFA.type=RollingRandomAccessFile >> conf/log4j2.properties
-    echo appender.DRFA.name=DRFA >> conf/log4j2.properties
-    echo appender.DRFA.fileName=\${log.dir}/\${log.file} >> conf/log4j2.properties
-    echo appender.DRFA.filePattern=\${log.dir}/\${log.file}.%d{yyyy-MM-dd} >> conf/log4j2.properties
-    echo appender.DRFA.layout.type=PatternLayout >> conf/log4j2.properties
-    echo appender.DRFA.layout.pattern=%d{DEFAULT} %-5p %c: [%t]: %m%n >> conf/log4j2.properties
-    echo appender.DRFA.policies.type=Policies >> conf/log4j2.properties
-    echo appender.DRFA.policies.size.type=SizeBasedTriggeringPolicy >> conf/log4j2.properties
-    echo appender.DRFA.policies.size.size=\${max.log.file.size} >> conf/log4j2.properties
-    echo appender.DRFA.strategy.type=DefaultRolloverStrategy >> conf/log4j2.properties
-    echo appender.DRFA.strategy.max=\${max.log.file.backup.index} >> conf/log4j2.properties
-    echo appender.console.type=Console >> conf/log4j2.properties
-    echo appender.console.name=console >> conf/log4j2.properties
-    echo appender.console.target=SYSTEM_ERR >> conf/log4j2.properties
-    echo appender.console.layout.type=PatternLayout >> conf/log4j2.properties
-    echo appender.console.layout.pattern=%d{yy/MM/dd HH:mm:ss} %-5p %c{2}: [%t]: %m%n >> conf/log4j2.properties
+    prepare_log_conf
     exec ${KYUUBI_HOME}/bin/kyuubi run
     ;;
   (*)
